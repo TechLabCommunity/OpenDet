73d7f3f685b003d5b9189a4211112f415403ff0b
DESKTOP-DDP1EJN\mikyb <brunellim94@gmail.com>
2017-12-17 23:21:26 +0100

    Inserita definizione DEBUGBEGIN

--- lib/Utility/Utility.h
+++ lib/Utility/Utility.h
@@ -1,9 +1,11 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
-#ifdef DEBUG
+#ifdef DEBUGGER
+  #define DEBUGBEGIN(b) Serial.begin(b)
   #define DEBUG(s) Serial.print(s)
 #else
+  #define DEBUGBEGIN(b)
   #define DEBUG(s)
 #endif
 
