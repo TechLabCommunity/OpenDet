83851355eaa82ef2520ad6473758b5f3cc7a87b9
Michele Brunelli <brunellim94@gmail.com>
2018-05-14 13:19:24 +0200

    Formatting changed

--- src/test/hardware/detscreen/main.cpp
+++ src/test/hardware/detscreen/main.cpp
@@ -1,7 +1,8 @@
-#include <Global.h>
 #include <DetScreen.h>
+#include <Global.h>
 
 DetScreen scr(0x27, 20, 4);
+// LiquidCrystal_I2C scr(0x27, 20, 4);
 unsigned long timer1 = 0;
 unsigned long timer2 = 0;
 uint coin = 0;
@@ -9,6 +10,7 @@ bool is_first = true;
 
 void setup() {
   DEBUGBEGIN(115200);
+
   scr.begin();
   scr.backlight();
   scr.start_system(VERSION_FIRMWARE);
@@ -17,19 +19,18 @@ void setup() {
 }
 
 void loop() {
-  if (millis() - timer2 < 20000){
-    if (millis() - timer1 >= 5000){
-      coin+=70;
+    if (millis() - timer2 < 20000) {
+    if (millis() - timer1 >= 5000) {
+      coin += 70;
       timer1 = millis();
     }
     scr.main_screen(0, coin);
-  }else{
-    if (is_first){
+  } else {
+    if (is_first) {
       scr.clear();
       is_first = false;
     }
 
     scr.dispense_screen("");
   }
-
 }
