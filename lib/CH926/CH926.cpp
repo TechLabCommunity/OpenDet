b02fb33f020effe1cc5e28302337716a9e390ede
Michele Brunelli <brunellim94@gmail.com>
2018-05-14 12:56:29 +0200

    Removed useless comment
    Removed useless comment

--- lib/CH926/CH926.cpp
+++ lib/CH926/CH926.cpp
@@ -1,7 +1,7 @@
 #include "CH926.h"
+#include <Global.h>
 
-CH926::CH926(int sigPin, int pwrPin, int sigLogic, int pwrLogic){
-
+CH926::CH926(int sigPin, int pwrPin, int sigLogic, int pwrLogic) {
   pinMode(sigPin, INPUT);
   pinMode(pwrPin, OUTPUT);
   digitalWrite(pwrPin, !pwrLogic);
@@ -11,42 +11,34 @@ CH926::CH926(int sigPin, int pwrPin, int sigLogic, int pwrLogic){
   _pwrLogic = pwrLogic;
   _sigLogic = sigLogic;
   _state = 0;
-
 }
 
-void CH926::start(){
+void CH926::start() {
   digitalWrite(_pwrPin, _pwrLogic);
   _state = 1;
 }
 
-void CH926::stop(){
+void CH926::stop() {
   digitalWrite(_pwrPin, !_pwrLogic);
   _state = 0;
 }
 
-/*int CH926::countCoin(){
-  if (digitalRead(_sigPin) != _sigLogic) {
-    countPulses();
-  }
-  else {
-
-  }
-}*/
-
-int CH926::countPulses(unsigned long endTime){
+int CH926::countPulses(unsigned long endTime) {
   int currState = _sigLogic;
   int prevState = _sigLogic;
   int counter = 0;
   unsigned long prevTime = millis();
 
   if (_state) {
-    //if millis() overflow reset prevTime
-    if(millis() - prevTime < 0){
+    // if millis() overflow reset prevTime
+    if (millis() - prevTime < 0) {
       prevTime = millis();
     }
 
-    while(millis() - prevTime < endTime){
-      if(currState != prevState){
+    while ((millis() - prevTime) < endTime) {
+      currState = digitalRead(_sigPin);
+
+      if (currState != prevState) {
         prevState = currState;
         counter++;
         prevTime = millis();
