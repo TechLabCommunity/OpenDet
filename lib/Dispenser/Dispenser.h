4b7afd61fe61c410098c282969d6c9d0392e1cf4
Michele Brunelli <brunellim94@gmail.com>
2018-05-14 13:10:20 +0200

    Formatting changed

--- lib/Dispenser/Dispenser.h
+++ lib/Dispenser/Dispenser.h
@@ -1,7 +1,8 @@
 /*
  * FlowSensor.h - Library for XXXX flow sensors
  * Created by Michele Brunelli on 12 dec 2017
- * Ask the creator if you want to use this library, email me at brunellim94@gmail.com
+ * Ask the creator if you want to use this library, email me at
+ * brunellim94@gmail.com
  */
 
 #ifndef DISPENSER_H
@@ -9,26 +10,22 @@
 
 #include <Global.h>
 
-//defined in millilitres.
-enum FIXED_QUANTITY{
-  LT = 1000, HALFLT = 500, QTYLT = 250
-};
+// defined in millilitres.
+enum FIXED_QUANTITY { LT = 1000, HALFLT = 500, QTYLT = 250 };
 
-class Dispenser{
+class Dispenser {
+ public:
+  Dispenser(uint, uint, uint, uint, String, uint);
+  uint getPulses();
+  String getDetName();
+  uint getPrice();
+  void dispense(FIXED_QUANTITY);
+  void dispense(uint = 1000);
 
-  public:
-    Dispenser(uint, uint, uint, uint, String, uint);
-    uint getPulses();
-    String getDetName();
-    uint getPrice();
-    void dispense(FIXED_QUANTITY);
-
-  protected:
-    void dispense(uint = 1);
-
-  private:
-    uint _pulsesPerLiter, _btnPin, _flowPin, _pumpPin, _detPrice;
-    String _detName;
+ protected:
+ private:
+  uint _pulsesPerLiter, _btnPin, _flowPin, _pumpPin, _detPrice;
+  String _detName;
 };
 
 #endif
