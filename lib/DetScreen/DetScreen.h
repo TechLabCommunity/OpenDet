b977541534b4ca233d5d86344c5f79b08765bb90
Michele Brunelli <brunellim94@gmail.com>
2018-05-14 12:48:15 +0200

    smartprint() as public method Added other screen - dispenseEnd, creditLow

--- lib/DetScreen/DetScreen.h
+++ lib/DetScreen/DetScreen.h
@@ -4,38 +4,35 @@
 #include <Global.h>
 #include "LiquidCrystal_I2C.h"
 
-class DetScreen : public LiquidCrystal_I2C{
+class DetScreen : public LiquidCrystal_I2C {
+ private:
+  unsigned int address, cols, rows;
+  unsigned long timer = 0;
+  uint slider = 0;
 
-  private:
+ protected:
+  unsigned int inline calcol(const String&);
 
-    unsigned int address, cols, rows;
-    unsigned long timer = 0;
-    uint slider = 0;
+  static String cents_to_string(const uint&);
 
-  protected:
+  void slideprint(const String&, uint);
 
-    unsigned int inline calcol(const String&);
+  void blinkprint(const String&, uint);
 
-    void inline smartprint(const String&, uint);
+ public:
+  DetScreen(uint, uint, uint);
 
-    static String cents_to_string(const uint&);
+  void smartprint(const String&, uint);
 
-    void slideprint(const String&, uint);
+  void start_system(const String& version);
 
-    void blinkprint(const String&, uint);
+  void main_screen(uint, uint);
 
-  public:
+  void dispense_screen(const String&);
+  void dispenseEnd_screen();
+  void creditLow_screen(uint, uint);
 
-    DetScreen(uint, uint, uint);
-
-    void start_system(const String& version);
-
-    void main_screen(uint, uint);
-
-    void dispense_screen(const String&);
-
-    void clear();
-
+  void clear();
 };
 
 #endif
