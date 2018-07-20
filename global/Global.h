d32b822d9ea465e6163e027d3c371c7dd3555dc4
Michele Brunelli <brunellim94@gmail.com>
2018-07-20 09:07:55 +0200

    VS code formatted

--- global/Global.h
+++ global/Global.h
@@ -2,33 +2,34 @@
 #define UTILITY_H
 
 #ifdef FORCE_ARDUINO
-  #include <Arduino.h>
+#include <Arduino.h>
 #else
-  #include <Controllino.h>
+#include <Controllino.h>
 #endif
 
-//Depending on DEBUGGER build_flags in platformio.ini
+// Depending on DEBUGGER build_flags in platformio.ini
 #ifdef DEBUGGER
-  #define DEBUGBEGIN(b) (Serial.begin(b))
-  #define DEBUG(s) (Serial.print(s))
+#define DEBUGBEGIN(b) (Serial.begin(b))
+#define DEBUG(s) (Serial.print(s))
 #else
-  #define DEBUGBEGIN(b)
-  #define DEBUG(s)
+#define DEBUGBEGIN(b)
+#define DEBUG(s)
 #endif
 
 #define uint unsigned int
 
 #define VERSION_FIRMWARE "0.1a"
 
-#include <inttypes.h>
+#include <MsTimer2.h>
 #include <Print.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <MsTimer2.h>
+#include <inttypes.h>
 
-void inline SYSERR(String s){
-  DEBUG("SYSERR : "+s+"\n");
-  while(1){}
+void inline SYSERR(String s) {
+  DEBUG("SYSERR : " + s + "\n");
+  while (1) {
+  }
 }
 
 #endif
