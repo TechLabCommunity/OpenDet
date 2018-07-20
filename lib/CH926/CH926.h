254b8abb4a6ef0802600d52c508b32164dcde2f9
Michele Brunelli <brunellim94@gmail.com>
2018-07-20 09:09:27 +0200

    Added methods to use interrupt to count pulses

--- lib/CH926/CH926.h
+++ lib/CH926/CH926.h
@@ -1,7 +1,8 @@
 /*
  * CH926.h - Library for ch-926 coin acceptor
  * Created by Michele Brunelli on 16 dec 2017
- * Ask the creator if you want to use this library, email me at brunellim94@gmail.com
+ * Ask the creator if you want to use this library, email me at
+ * brunellim94@gmail.com
  */
 
 #ifndef CH926_H
@@ -12,22 +13,30 @@
 #define ACTIVE_LOW 0
 #define ACTIVE_HIGH 1
 
-#define NC 1
-#define NO 0
+#define NC 0
+#define NO 1
 
-class CH926{
-  public:
-    CH926(int, int, int, int);
-    void start();
-    void stop();
-    //int countCoin();
-    int countPulses(unsigned long);
-  private:
-    int _sigPin;
-    int _pwrPin;
-    int _sigLogic;
-    int _pwrLogic;
-    int _state;
+class CH926 {
+ public:
+  CH926(int, int, int, int);
+  void start();
+  void stop();
+  int countPulses(unsigned long);
+
+  void startInterrupt();
+  void stopInterrupt();
+  uint32_t counter();
+
+  static uint32_t _counter;
+
+ private:
+  int _sigPin;
+  int _pwrPin;
+  int _sigLogic;
+  int _pwrLogic;
+  int _state;
+  // TODO trovare modo di avere counter divisi per diverse gettoniere
+  static void ISR_counter();
 };
 
 #endif
